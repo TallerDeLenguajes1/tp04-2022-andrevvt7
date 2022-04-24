0e7e24a5ee7bffe686f2439f934926fd3e38d2a6
andrevvt7 <andrevvt7@gmail.com>
2022-04-24 19:53:14 -0300

    tarea por id en rama 2

--- ejercicio2.c
+++ ejercicio2.c
@@ -12,6 +12,7 @@ int Duracion; // entre 10 – 100
 void cargarListaDeTareas(tarea** tareasPorHacer, int cantidadDeTareas);
 void preguntarYCargarTareasRealizadas(tarea** tareasPorHacer, tarea** tareasRealizadas, int cantidadDeTareas);
 void mostrarTareasRealizadasYPendientes(tarea** tareasPorHacer, tarea** tareasRealizadas, int cantidadDeTareas);
+void buscarTarea(tarea** tareasPorHacer, tarea** tareasRealizadas, int cantidadDeTareas);
 
 int main(){
     srand(time(NULL));
@@ -26,6 +27,7 @@ int main(){
     cargarListaDeTareas(tareasPorHacer, cantidadDeTareas);
     preguntarYCargarTareasRealizadas(tareasPorHacer, tareasRealizadas, cantidadDeTareas);
     mostrarTareasRealizadasYPendientes(tareasPorHacer, tareasRealizadas, cantidadDeTareas);
+    buscarTarea(tareasPorHacer, tareasRealizadas, cantidadDeTareas);
 
     return 0;
 }
@@ -52,6 +54,9 @@ void cargarListaDeTareas(tarea** tareasPorHacer, int cantidadDeTareas){
 void preguntarYCargarTareasRealizadas(tarea** tareasPorHacer, tarea** tareasRealizadas, int cantidadDeTareas){
     char respuesta;
 
+    printf("_____________________________");
+    printf("\nCONSULTA POR TAREA\n");
+
     for (int i = 0; i < cantidadDeTareas; i++){
         tareasRealizadas[i] = NULL;
     }
@@ -74,8 +79,11 @@ void preguntarYCargarTareasRealizadas(tarea** tareasPorHacer, tarea** tareasReal
 }
 
 void mostrarTareasRealizadasYPendientes(tarea** tareasPorHacer, tarea** tareasRealizadas, int cantidadDeTareas){
-    
-    puts("\nTAREAS REALIZADAS:\n ");
+
+    printf("_____________________________");
+    printf("\nLISTAS DE TAREAS\n");
+
+    puts("\nTAREAS REALIZADAS: ");
     for (int i = 0; i < cantidadDeTareas; i++){
         if (tareasRealizadas[i] != NULL){
             printf("Tarea %d ", tareasRealizadas[i]->TareaID);
@@ -90,6 +98,30 @@ void mostrarTareasRealizadasYPendientes(tarea** tareasPorHacer, tarea** tareasRe
             printf("Tarea %d ", tareasPorHacer[i]->TareaID);
             printf("\nDescripci%cn: %s\n", 162,tareasPorHacer[i]->Descripcion);
             printf("Duraci%cn: %d\n\n", 162,tareasPorHacer[i]->Duracion);
+        }
+    }
+}
+
+void buscarTarea(tarea** tareasPorHacer, tarea** tareasRealizadas, int cantidadDeTareas){
+    int id;
+
+    printf("_____________________________");
+    printf("\nIngrese el ID de la tarea solicitada: ");
+    scanf("%d", &id);
+    fflush(stdin);
+
+    printf("TAREA POR ID ELEGIDO\n");
+
+    for(int i = 0 ; i < cantidadDeTareas ; i++){
+        if(tareasPorHacer[i] != NULL && tareasPorHacer[i]->TareaID == id){
+            printf("\nTarea %d ", tareasPorHacer[i]->TareaID);
+            printf("\nDescripci%cn: %s\n", 162,tareasPorHacer[i]->Descripcion);
+            printf("Duraci%cn: %d", 162,tareasPorHacer[i]->Duracion);
+        }
+        if(tareasRealizadas[i] != NULL && tareasRealizadas[i]->TareaID == id){
+            printf("\nTarea %d ", tareasRealizadas[i]->TareaID);
+            printf("\nDescripci%cn: %s\n", 162,tareasRealizadas[i]->Descripcion);
+            printf("Duraci%cn: %d", 162,tareasRealizadas[i]->Duracion);
         }
     }
 }
